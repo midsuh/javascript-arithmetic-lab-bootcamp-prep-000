1a7eaf01c8b586350ee39224faabdb058835a85e
irusaf@yahoo.com <irusaf@yahoo.com>
2018-09-01 21:40:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 function add() {
   
+  return 
 }
 
 function subtract() {
-  
+ 
+ return  
 }
 
 function multiply() {
   
+  return 
 }
 
 function divide() {
   
+  return 
 }
 
 function inc(n) {
   
-  
+  return 
 }
 
 function dec(n) {
   
-  
+  return 
 }
 
 function makeInt(n) {
   
-  
+  return 
 }
 
 function preserveDecimal(n) {
   
-  
+  return parseFloat(n)
 }
